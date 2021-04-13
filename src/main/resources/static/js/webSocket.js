2952d43133a23862ede00e740d3dfa7efe0d5344
T_2019 <291421358@qq.com>
2021-04-13 15:05:03 +0800

    websocket

--- src/main/resources/static/js/webSocket.js
+++ src/main/resources/static/js/webSocket.js
@@ -43,7 +43,7 @@ websocket.onmessage = function (event) {
 
 //连接关闭的回调方法
 websocket.onclose = function () {
-    setMessageInnerHTML("WebSocket连接关闭");
+    // setMessageInnerHTML("WebSocket连接关闭");
 };
 
 //监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。

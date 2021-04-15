1b8e3cd4ddccb8cfa18c50ef21dc93939dd3ce71
T_2019 <291421358@qq.com>
2021-04-15 16:41:24 +0800

    index

--- src/main/java/com/laola/apa/config/IndexConfig.java
+++ src/main/java/com/laola/apa/config/IndexConfig.java
@@ -23,17 +23,7 @@ public class IndexConfig {
         System.out.println("mini-100准备就绪 ... 启动浏览器");
 
 //        // 启动后访问地址
-//        String url = "http://localhost:8081";
-//        Runtime runtime = Runtime.getRuntime();
-//        try {
-//            runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
         String cmd = " cmd /c \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe\"  --app=http://localhost:8081";// "cmd /c start"+path+"/"+filename;
-        //String cmd = "cmd /c start F:/mysqlbackup/guohua/backup.bat";
-
         try {
             Runtime.getRuntime().exec(cmd);
         } catch(IOException ioe) {
@@ -41,4 +31,9 @@ public class IndexConfig {
         }
     }
 
+    @EventListener({ApplicationReadyEvent.class})
+    public void applicationReadyEvent1() {
+        System.out.println("mini-100准备就绪 ... 启动浏览器");
+
+    }
 }

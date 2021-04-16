128595209740fd010e418a39de04b2296a1c5a1c
T_2019 <291421358@qq.com>
2021-04-16 17:52:40 +0800

    1加入温度读数的参数处理功能 2加入参数处理的接口 3实现自定义调用实现类 4对自定义的实现类加入泛型和动态参数

---
+++ src/main/java/com/laola/apa/server/impl/PortDataDeal/P86.java
@@ -0,0 +1,43 @@
+package com.laola.apa.server.impl.PortDataDeal;
+
+import com.laola.apa.entity.EquipmentState;
+import com.laola.apa.server.EquipmentStateserver;
+import com.laola.apa.server.PortDataDealService;
+import com.laola.apa.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.DecimalFormat;
+
+public class P86 implements PortDataDealService<Object, String> {
+
+    @Autowired
+    EquipmentStateserver equipmentStateSever;
+
+    @Override
+    public Object deal(String... data) {
+        String string = data.toString();
+        //A12：进水标志   1/0代表进水
+        int pureWater = DateUtils.decodeHEX(string.substring(30, 32));
+        //A13：出水标志   1/0代表出水满
+        int wasteWater = DateUtils.decodeHEX(string.substring(32, 34));
+        //A15：撞针标志   1/0代表撞针
+        int firingPin = DateUtils.decodeHEX(string.substring(36, 38));
+
+        // A20：反应盘温度高位（计算方法：高位*256+高位）/1351（测试过程中返回有效）
+        //A21：反应盘温度低位（测试过程中返回有效）
+        float reactTemp = (Float.parseFloat(String.valueOf(DateUtils.decodeHEX(string.substring(46, 48)))) * 256F + Float.parseFloat(String.valueOf(DateUtils.decodeHEX(string.substring(48, 50))))) / 1351F;
+        //A24：试剂盘温度
+        int regentTemp = DateUtils.decodeHEX(string.substring(54, 56));
+        //A25：发送数据组数。
+        int numSent = DateUtils.decodeHEX(string.substring(56, 58));
+        //A27：在测试的项目数
+        int numberUnderTest = DateUtils.decodeHEX(string.substring(60, 62));
+        //A28：所有项目数
+        int numAll = DateUtils.decodeHEX(string.substring(62, 64));
+//        logger.info("string==" + string);
+        EquipmentState equipmentState = new EquipmentState(1, pureWater, wasteWater, firingPin, String.valueOf(new DecimalFormat("0.00").format(reactTemp)), String.valueOf(regentTemp), numSent, numberUnderTest, numAll);
+//        logger.info("equipment=" + equipmentState.toString());
+        EquipmentState update = equipmentStateSever.update(equipmentState);
+        return null;
+    }
+}

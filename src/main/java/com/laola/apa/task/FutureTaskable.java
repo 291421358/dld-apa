bcc0d862e68ccbc9c7b474e771ebd5d6cf6bedce
T_2019 <291421358@qq.com>
2021-04-23 14:55:55 +0800

    添加线程名称 用于停止线程 更换包位置

--- src/main/java/com/laola/apa/task/FutureTaskable.java
+++ src/main/java/com/laola/apa/task/FutureTaskable.java
@@ -1,4 +1,4 @@
-package com.laola.apa.utils.thread;
+package com.laola.apa.task;
 
 import com.laola.apa.utils.SerialUtil;
 
@@ -11,6 +11,9 @@ public class FutureTaskable   {
     public static FutureTask<?> futureTask = new FutureTask<Long>(new Callable<Long>() {
         @Override
         public Long call() throws Exception {
+            Thread.currentThread().setName("t_1");
+            String name = Thread.currentThread().getName();
+            System.out.println("线程名："+name);
             for(int i=0;i < 10000&&!Thread.currentThread().isInterrupted();i++){
                 SerialUtil.sendCommond("E5 90 83 01 00 00 00 00 00 00 00 00 00 00 00 00");
                 Thread.sleep(15000);
@@ -35,10 +38,26 @@ public class FutureTaskable   {
         System.out.println("futureTask cancel");
     }
 
+    /**
+     * 停止一个线程
+     */
     public static void stop(){
+        ThreadGroup currentGroup = Thread.currentThread().getThreadGroup();
 
-        executor.shutdownNow();
-        futureTask.cancel(true);
+        int noThreads = currentGroup.activeCount();
+
+        Thread[] lstThreads = new Thread[noThreads];
+
+        currentGroup.enumerate(lstThreads);
+
+        for (int j = 0; j < noThreads; j++) {
+
+            String nm = lstThreads[j].getName();
+            if (nm.equals("t_1")) {
+                lstThreads[j].stop();
+                System.out.println("停止线程："+nm);
+            }
+        }
     }
 
 }

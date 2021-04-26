f82f73170c2a938790f8e9b343dd8b3fc1ca30fd
T_2019 <291421358@qq.com>
2021-04-26 13:51:09 +0800

    日志

--- src/main/java/com/laola/apa/config/IndexConfig.java
+++ src/main/java/com/laola/apa/config/IndexConfig.java
@@ -1,6 +1,8 @@
 package com.laola.apa.config;
 
 import com.laola.apa.task.FutureTaskable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -18,11 +20,11 @@ import java.io.IOException;
  */
 @Configuration
 public class IndexConfig {
+    private Logger logger = LoggerFactory.getLogger(IndexConfig.class);
 
     @EventListener({ApplicationReadyEvent.class})
     public void applicationReadyEvent() {
-        System.out.println("mini-100准备就绪 ... 启动浏览器");
-
+        logger.info("mini-100准备就绪 ... 启动浏览器");
 //        // 启动后访问地址
         String cmd = " cmd /c \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe\"  --app=http://localhost:8081";// "cmd /c start"+path+"/"+filename;
         try {

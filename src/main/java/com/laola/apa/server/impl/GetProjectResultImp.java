4bce8faa5a40e2d181fe85b26ccfe5262fb81a96
T_2019 <291421358@qq.com>
2021-05-08 17:26:53 +0800

    架号位号光准 实现类 二维码优化

--- src/main/java/com/laola/apa/server/impl/GetProjectResultImp.java
+++ src/main/java/com/laola/apa/server/impl/GetProjectResultImp.java
@@ -64,13 +64,8 @@ public class GetProjectResultImp implements GetProjectResult {
         String s34 = string.substring(2, 4);
         if ("92".equals(s34)) {
             logger.info("接收到ad数据" + string);
-            try {
-                readAD(serialPort.getInputStream(), "user1", WebSocket.getClients().get("user1"), string);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            PortDataDealService<String,String> beanByName =  SpringBeanUtil.getBeanByTypeAndName(PortDataDealService.class,"p" + s34);
+            beanByName.deal(string);
             return;
         }
 
@@ -89,13 +84,8 @@ public class GetProjectResultImp implements GetProjectResult {
 
         if ("93".equals(s34) || string.length() > 2000) {
             logger.info("接收到光准数据" + string);
-            try {
-                readLightQuasi(serialPort.getInputStream(), "user1", WebSocket.getClients().get("user1"), string);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            PortDataDealService<String,String> beanByName =  SpringBeanUtil.getBeanByTypeAndName(PortDataDealService.class,"p" + s34);
+            beanByName.deal(string);
             return;
         }           //101-130       //20-40       //50-200
         //e5 90 128 (空杯子从哪拿) （抽标准液位子） （量）
@@ -105,7 +95,7 @@ public class GetProjectResultImp implements GetProjectResult {
         }
         String temp = "";
 
-        if ("9f".equals(s34)) {
+        if ("94".equals(s34)) {
             logger.info("项目二维码信息" + string);
             dealQR(string);
         }
@@ -129,7 +119,6 @@ public class GetProjectResultImp implements GetProjectResult {
         if (DateUtils.decodeHEX(string.substring(4, 6)) <= 80 && string.length() > 64 && "91".equals(s34)) {
             logger.info("接收到结果数据" + string);
             PortDataDealService<String,String> beanByName =  SpringBeanUtil.getBeanByTypeAndName(PortDataDealService.class,"p" + s34);
-
             beanByName.deal(string);
         }
         if (!"".equals(temp)) {
@@ -138,7 +127,9 @@ public class GetProjectResultImp implements GetProjectResult {
         s34 = string.substring(2, 4);
         if ("9c".equals(s34)) {
             logger.info("接收到位号和二维码信息" + string);
-            deal9cCommand(string, serialPort);
+            PortDataDealService<String,Object> beanByName =  SpringBeanUtil.getBeanByTypeAndName(PortDataDealService.class,"p" + s34);
+            beanByName.deal(string,serialPort);
+//            deal9cCommand(string, serialPort);
         }
 
 
@@ -235,157 +226,7 @@ public class GetProjectResultImp implements GetProjectResult {
      * @throws IOException
      */
     private void deal9cCommand(String string, SerialPort serialPort) {
-        //架号
-        Integer rackNo = DateUtils.decodeHEX(string.substring(4, 6));
-        //位号
-        Integer placeNo = DateUtils.decodeHEX(string.substring(6, 8));
-        //条形码scan_back
-        String barCode = DateUtils.hexAscii2Str(string.substring(10, string.length() - 2));
-        //
-        EquipmentState equipmentState = new EquipmentState(1, rackNo, placeNo);
-        equipmentStateSever.update(equipmentState);
 
-        //查询有效的项目
-        List<Map<String, Object>> ableList = projectTest.selectNeverDo();
-        for (Map<String, Object> ablemap : ableList) {
-            //循环有效项目
-            if ((null != ablemap.get("rackNo") && Integer.parseInt(String.valueOf(ablemap.get("placeNo"))) == placeNo &&
-                    rackNo == Integer.parseInt(String.valueOf(ablemap.get("rackNo")))) && !"2".equals(String.valueOf(ablemap.get("type")))) {
-                //架号位号相同，进入发送功能，取得命令 根据项目id
-                String commond = projectTest.getIdList(Integer.parseInt(String.valueOf(ablemap.get("id"))));
-                //病员信息
-                if ("1".equals(String.valueOf(ablemap.get("type")))){
-                    Patient patient = new Patient(Integer.parseInt(String.valueOf(ablemap.get("human_code"))), "É".equals(barCode)?"":barCode);
-                    //修改病员信息
-                    patientService.update(patient);
-                }
-                //位号为一位，加上0 变成两个字符
-                String strPlaceNo = String.valueOf(placeNo);
-                if (strPlaceNo.length() == 1) {
-                    strPlaceNo = "0" + strPlaceNo;
-                }
-                //
-                Integer regentPlace = usedCodeServer.getCopies(Integer.parseInt(String.valueOf(ablemap.get("id"))));
-
-                Project project = new Project();
-                project.setId(Integer.parseInt(String.valueOf(ablemap.get("id"))));
-                if (regentPlace<=0){
-                    project.setAbnormal(1);
-                }else if (regentPlace<= 5){
-                    project.setAbnormal(2);
-                }
-                projectMapper.updateByPrimaryKeySelective(project);
-                project = null;
-                commond = TestConstant.TestHead + strPlaceNo + " " + commond + " 00 00";
-                logger.info("发出的命令 123：" + commond);
-                OutputStream outputStream = null;
-                try {
-                    outputStream = serialPort.getOutputStream();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                byte[] binaryStr = DateUtils.hexStrToBinaryStr(commond);
-                assert binaryStr != null;
-                try {
-                    outputStream.write(binaryStr, 0, binaryStr.length);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            if ("2".equals(String.valueOf(ablemap.get("type"))) && (null != ablemap.get("rackNo") && Integer.parseInt(String.valueOf(ablemap.get("placeNo"))) == placeNo &&
-                    rackNo == Integer.parseInt(String.valueOf(ablemap.get("rackNo"))))) {
-                String commond = projectTest.getIdList(Integer.parseInt(String.valueOf(ablemap.get("id"))));
-                String strPlaceNo = String.valueOf(placeNo);
-                if (strPlaceNo.length() == 1) {
-                    strPlaceNo = "0" + strPlaceNo;
-                }
-                commond = TestConstant.TestHead + strPlaceNo + " " + commond + " 00 00";
-                logger.info("发出的命令；" + commond);
-                OutputStream outputStream = null;
-                try {
-                    outputStream = serialPort.getOutputStream();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                byte[] binaryStr = DateUtils.hexStrToBinaryStr(commond);
-                assert binaryStr != null;
-                try {
-                    outputStream.write(binaryStr, 0, binaryStr.length);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
-    }
-
-
-
-
-
-
-
-    /**
-     * 读取ad
-     *
-     * @param inputStream
-     * @param username
-     * @param webSocket
-     * @param hexStr
-     * @throws InterruptedException
-     * @throws IOException
-     */
-    private void readAD(InputStream inputStream, String username, WebSocket webSocket, String hexStr) throws InterruptedException, IOException {
-
-        logger.info("转码后的数据" + hexStr);
-
-        Map<String, String> ADMap = new HashMap<>(11);
-        assert hexStr != null;
-        if (!hexStr.substring(6, 8).equals("00")) {
-            ADMap.put("tem",
-                    String.valueOf((
-                            new BigInteger(hexStr.substring(4, 6), 16).intValue() * 256
-                                    + new BigInteger(hexStr.substring(6, 8), 16).intValue()) / 1351
-                    )
-            );
-        }
-        //取前72位数
-        hexStr = hexStr.substring(64);
-        for (int i = 0; i < hexStr.length() / 4; i++) {
-            ADMap.put("ad" + String.valueOf(i + 1),
-                    String.valueOf(
-                            new BigInteger(hexStr.substring(i * 4, i * 4 + 2), 16).intValue() * 256
-                                    + new BigInteger(hexStr.substring(i * 4 + 2, i * 4 + 4), 16).intValue()
-                    )
-            );
-        }
-//
-        ADMap.put("id", "readAD");
-        Gson gson = new Gson();
-        webSocket.sendMessageTo(gson.toJson(ADMap), username);
-    }
-
-    /**
-     * 读光准
-     *
-     * @param inputStream
-     * @param username
-     * @param webSocket
-     */
-    private void readLightQuasi(InputStream inputStream, String username, WebSocket webSocket, String hexStr) throws IOException, InterruptedException {
-        Map<String, Object> lightQuasiMap = new HashMap<>(11);
-        assert hexStr != null;
-        List<String> LightQuasiStr = new ArrayList<>();
-        for (int i = 0; i < hexStr.length() / 2; i++) {
-            BigInteger bigInteger = new BigInteger(hexStr.substring(i * 2, i * 2 + 2), 16);
-            if (i % 5 != 0 || (Integer.parseInt(String.valueOf(bigInteger)) > 250)) {
-                LightQuasiStr.add(String.valueOf(bigInteger));
-            }
-        }
-        lightQuasiMap.put("msg", LightQuasiStr);
-        lightQuasiMap.put("id", "readLightQuasi");
-        Gson gson = new Gson();
-        webSocket.sendMessageTo(gson.toJson(lightQuasiMap), username);
     }
 
 }

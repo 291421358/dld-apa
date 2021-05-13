1ca6737014ae54457da78dcd187108e15ade0788
T_2019 <291421358@qq.com>
2021-05-13 16:26:03 +0800

    添加注释

--- src/main/java/com/laola/apa/server/ScalingIntf.java
+++ src/main/java/com/laola/apa/server/ScalingIntf.java
@@ -14,17 +14,52 @@ public interface ScalingIntf {
      */
     List<Map<String, Object>> selectOneCurve(int id);
 
+    /**
+     * @apiNote
+     * @author tzhh
+     * @date 2021/5/13 11:05
+     * @param id
+     * @return {@link String}
+     **/
     String getFactor(int id);
 
+    /**
+     * @apiNote
+     * @author tzhh
+     * @date 2021/5/13 11:05
+     * @param projectParamId
+     * @return {@link List< Map< String, Object>>}
+     **/
     List<Map<String, Object>> getLatestOne(Integer projectParamId);
 
-    //根据条件获得定标项目值
-    Map<String, List> getOneProjectsAllScalingByCon(int id, int type, String time);
     /**
-     * 修改定标项目
-     */
+     * @apiNote
+     * @author tzhh
+     * @date 2021/5/13 11:07
+     * @param id
+	 * @param type
+	 * @param time
+     * @return {@link Map< String, List>}
+     **/
+    Map<String, List> getOneProjectsAllScalingByCon(int id, int type, String time);
+    /***
+     * @apiNote
+     * @author tzhh
+     * @date 2021/5/13 11:05
+     * @param projectParamId
+     * @param projectId
+     * @return {@link int}
+     **/
     int updateProjectsScaling(int projectParamId, String projectId);
 
+    /***
+     * @apiNote 
+     * @author tzhh 
+     * @date 2021/5/13 11:04
+     * @param projectParamId
+     * @param i
+     * @return {@link List< Map< String, Object>>}
+     **/
     List<Map<String, Object>> getOneProjectsScalingTime(int projectParamId, int i);
 
     /**
@@ -32,12 +67,25 @@ public interface ScalingIntf {
      */
     int updateProject(Project project);
 
+    /***
+     * @apiNote 添加定标
+     * @author tzhh 
+     * @date 2021/5/13 11:04
+     * @param scaling
+     * @return {@link int}
+     **/
     int insertScalingAlgorithm(Scaling scaling);
 
     /**
      *修改曲线 updateProjectParam
      */
     int updateScalingAlgorithm(Scaling scaling);
-
+    /**
+     * @apiNote
+     * @author tzhh
+     * @date 2021/5/13 11:07
+     * @param dateId
+     * @return {@link int}
+     **/
     int deleteOneScaling(String dateId);
 }

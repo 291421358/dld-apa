e3409cef6e121497a02c64489631bf5f9908bfdb
T_2019 <291421358@qq.com>
2021-06-01 15:38:48 +0800

    光准  散射+10个

--- src/main/java/com/laola/apa/costant/LightQuasiConstant.java
+++ src/main/java/com/laola/apa/costant/LightQuasiConstant.java
@@ -7,14 +7,16 @@ public class LightQuasiConstant extends BaseCostant{
     public static final Map<String,String> lightQuasiMap = new HashMap<String, String>(){
         {
             put("READ_LIGHTQUASI","01 01 00 ");
-            put("READ_LIGHTQUASI_01","01 01 00 ");
-            put("READ_LIGHTQUASI_02","02 01 00 ");
-            put("READ_LIGHTQUASI_03","03 01 00 ");
-            put("READ_LIGHTQUASI_04","04 01 00 ");
-            put("READ_LIGHTQUASI_05","05 01 00 ");
-            put("READ_LIGHTQUASI_06","06 01 00 ");
-            put("READ_LIGHTQUASI_07","07 01 00 ");
-            put("READ_LIGHTQUASI_08","08 01 00 ");
+            put("READ_LIGHTQUASI_01","01 00 00 ");
+            put("READ_LIGHTQUASI_02","01 01 00 ");
+            put("READ_LIGHTQUASI_03","01 02 00 ");
+            put("READ_LIGHTQUASI_04","01 03 00 ");
+            put("READ_LIGHTQUASI_05","01 04 00 ");
+            put("READ_LIGHTQUASI_06","01 05 00 ");
+            put("READ_LIGHTQUASI_07","01 06 00 ");
+            put("READ_LIGHTQUASI_08","01 07 00 ");
+            put("READ_LIGHTQUASI_09","01 08 00 ");
+            put("READ_LIGHTQUASI_10","01 09 00 ");
         }
     };
 }

768f580583f470cd97ba44c72993564e8e38e5f6
T_2019 <291421358@qq.com>
2021-08-02 08:52:03 +0800

    删除稀释试剂

--- src/main/java/com/laola/apa/server/impl/UsedCodeImpl.java
+++ src/main/java/com/laola/apa/server/impl/UsedCodeImpl.java
@@ -101,7 +101,13 @@ public class UsedCodeImpl implements UsedCodeServer {
     public int minusOneCopyReagent(int id) {
         usedCodeMapper.minusOneCopyReagentR1(id);
         usedCodeMapper.minusOneCopyReagentR2(id);
-        usedCodeMapper.minusOneCopyReagentX(id);
+        try{
+
+            usedCodeMapper.minusOneCopyReagentX(id);
+            usedCodeMapper.minusOneCopyReagentX2(id);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return 0;
     }
 

05aa20367d83d71cc1058364e7979eb1eb3bb639
T_2019 <291421358@qq.com>
2021-08-19 13:14:02 +0800

    添加时间格式转换

--- src/main/java/com/laola/apa/config/IndexConfig.java
+++ src/main/java/com/laola/apa/config/IndexConfig.java
@@ -1,8 +1,10 @@
 package com.laola.apa.config;
 
+import com.laola.apa.server.ProjectTest;
 import com.laola.apa.task.FutureTaskable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -21,9 +23,14 @@ import java.io.IOException;
 @Configuration
 public class IndexConfig {
     private Logger logger = LoggerFactory.getLogger(IndexConfig.class);
-
+    @Autowired
+    private ProjectTest projectTest;
     @EventListener({ApplicationReadyEvent.class})
     public void applicationReadyEvent() {
+
+        logger.info("删除所有未做的项目");
+        projectTest.deleteProjects();
+
         logger.info("mini-100准备就绪 ... 启动浏览器");
 //        // 启动后访问地址
         String cmd = " cmd /c \"C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe\"  --app=http://localhost:8081";// "cmd /c start"+path+"/"+filename;

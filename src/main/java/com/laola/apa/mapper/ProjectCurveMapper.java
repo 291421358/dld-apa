77969586a2c1bb5cab857eace784a9926f4d23f8
T_2019 <291421358@qq.com>
2021-09-26 10:13:46 +0800

    //获取第一个     Integer get1st(Integer pid);     //获取加试剂2的     Float getAddR2(Integer pid);     //获取最后一个     Float getlast(Integer pid);

--- src/main/java/com/laola/apa/mapper/ProjectCurveMapper.java
+++ src/main/java/com/laola/apa/mapper/ProjectCurveMapper.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface ProjectCurveMapper extends MyMapper<ProjectCurve> {
+
+    //获取第一个
+    Integer get1st(Integer pid);
+    //获取加试剂2的
+    Float getAddR2(Integer pid);
+    //获取最后一个
+    Float getlast(Integer pid);
+
 }
